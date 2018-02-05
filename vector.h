6f393e4850820a1940bb12e036e2be449dc4717e
Jesse Millar <hellojessemillar@gmail.com>
2018-02-05 12:00:01 -0800

    Simplifying vectors

--- vector.h
+++ vector.h
@@ -1,47 +1,29 @@
 // minimal class to replace std::vector
-template<typename T, unsigned int MaxCapacity>
-class Vector
+template<typename T, int Capacity>
+struct Vector
 {
-	unsigned int d_size; // stores the number of actually stored objects
-	unsigned int d_capacity; // stores allocated capacity
-	unsigned int d_max_capacity; // prevent the vector from eating up all the memory
+	int d_size = 0; // stores the number of actually stored objects
+	int d_capacity; // stores allocated capacity
 	T *d_data; // stores the actual data
 
-public:
 	// default constructor
-	Vector() : d_size(0), d_capacity(0), d_max_capacity(MaxCapacity), d_data(0) {};
-
-	// copy constuctor
-	Vector(Vector const &other) : d_size(other.d_size), d_capacity(other.d_capacity), d_max_capacity(other.d_max_capacity), d_data(0)
+	Vector()
 	{
+		d_capacity = Capacity;
 		d_data = (T *)malloc(d_capacity * sizeof(T));
-		memcpy(d_data, other.d_data, d_size * sizeof(T));
-	};
+	}
 
 	// destructor
 	~Vector()
 	{
 		free(d_data);
-	};
-
-	// needed for memory management
-	Vector &operator=(Vector const &other)
-	{
-		free(d_data);
-		d_size = other.d_size;
-		d_capacity = other.d_capacity;
-		d_max_capacity = other.d_max_capacity;
-		d_data = (T *)malloc(d_capacity * sizeof(T));
-		memcpy(d_data, other.d_data, d_size * sizeof(T));
-		return *this;
-	};
+	}
 
 	// adds a new value and allocates more space if needed (and allowed)
 	void push_back(T const &x)
 	{
-		if (d_capacity < d_max_capacity && d_capacity == d_size) resize();
 		if (d_size < d_capacity) d_data[d_size++] = x;
-	};
+	}
 
 	// wipe the array
 	void clear()
@@ -50,7 +32,7 @@ public:
 	}
 
 	// size getter
-	unsigned int size() const
+	int size() const
 	{
 		return d_size;
 	};
@@ -58,7 +40,7 @@ public:
 	// delete an index
 	void erase(int index)
 	{
-		for (int i = index; i < d_size; ++i)
+		for (int i = index; i < d_size; i++)
 		{
 			d_data[i] = d_data[i + 1]; // copy next element left
 		}
@@ -67,31 +49,14 @@ public:
 	}
 
 	// const getter
-	T const &operator[](unsigned int idx) const
+	T const &operator[](int idx) const
 	{
 		return d_data[idx];
-	};
+	}
 
 	// changeable getter
-	T &operator[](unsigned int idx)
+	T &operator[](int idx)
 	{
 		return d_data[idx];
-	};
-
-private:
-	// allocates double the old space while respecting d_max_capacity
-	void resize()
-	{
-		d_capacity = d_capacity ? d_capacity * 2 : 1;
-
-		if (d_capacity > d_max_capacity)
-		{
-			d_capacity = d_max_capacity;
-		}
-
-		T *newdata = (T *)malloc(d_capacity * sizeof(T));
-		memcpy(newdata, d_data, d_size * sizeof(T));
-		free(d_data);
-		d_data = newdata;
-	};
+	}
 };
