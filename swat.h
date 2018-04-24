1db221fe76f7bbd8283d93d79f186e2aabab7340
Jesse Millar <hellojessemillar@gmail.com>
2018-04-24 11:57:38 -0500

    SWAT spawn spikes

--- swat.h
+++ swat.h
@@ -25,12 +25,17 @@ struct Swat: Police
 		updateCbox();
 		updateAngle();
 		accelerate();
+
+		if (random(10) < 3)
+		{
+			if (!spikes.full()) spikes.add(Spike(x, y));
+		}
 	}
 
 	void draw() const
 	{
 		arduboy.fillRect(int16_t(x - camera.x + 4), int16_t(y - camera.y + 4), width - 8, height - 7, BLACK); // ghetto mask
 		sketch(SWAT, curFrame);
-		flash(-4, -7);
+		flash(-4, -8);
 	}
 };
